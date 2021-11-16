2214d40c3f54d4fbd671d0e385d96d56d3238479
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-16 09:45:35 +0800

    variables function server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// variables function
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const validate = require('./utils/validate');
+
