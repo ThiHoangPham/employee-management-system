7c368357e52d2b1d85211dfec9651e11d1459081
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-16 10:34:53 +0800

    coded choices menu server.js

--- server.js
+++ server.js
@@ -17,3 +17,34 @@ connection.conmect((error) => {
     console.log(chalk.blueBright.bold(`====================================================================================`));
     promptUser();
 });
+
+// Choices menu
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            name: 'choices',
+            type: 'list',
+            message: 'Please select an option:',
+            choices: [
+                'View All Employees',
+                'View All Roles',
+                'View All Departments',
+                'View All Employees By Department',
+                'View Department Budgets',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'Add Employee',
+                'Add Role',
+                'Add Department',
+                'Remove Employee',
+                'Remove Role',
+                'Remove Department',
+                'Exit'
+            ]
+        }
+    ])
+        .then((answers) => {
+            const { choices } = answers;
+            if { choices }
+        })
+}
